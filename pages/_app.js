4c0cb0811b8fb541861229da19119392cd6f7fd9
Eugene-Lek <58066799+Eugene-Lek@users.noreply.github.com>
2023-08-24 13:27:14 +0800

    add: vercel analytics module

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/general.sass'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
