2ecf1d80a26a6456bd095bbc1c06ac014bdc1b9f
Eugene-Lek <58066799+Eugene-Lek@users.noreply.github.com>
2023-09-13 20:58:31 +0800

    update: put user page behind password

---
+++ pages/[unit]/index.js
@@ -0,0 +1,105 @@
+import Head from 'next/head'
+import { useState, useEffect } from 'react'
+import { Header } from "src/components/header/header"
+import { Dialog } from "src/components/home/dialog"
+import ServicemanDetailsForm from '@/src/components/forms/servicemandetailsform'
+
+
+export default function Home({ selected_unit, unit_data }) {
+
+    const [dialog_settings, set_dialog_settings] = useState({
+        "message": '',
+        "buttons": [],
+        "line_props": [],
+        "displayed": false,
+        "onClickDialog": function () { return },
+        "onClickDialogProps": {}
+    })
+
+    return (
+        <>
+            <Head>
+                <title>T&T Template Generator</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="robots" content="noindex,nofollow" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main>
+                <Header title={"T&T Template Generator"} subtitle={"Write Well-Written, Well-Organised, and Personalised T&Ts"} />
+                <section className="big-section">
+                    <h2 className="big-section-title">Provide Your Serviceman&apos;s Details</h2>
+                    <ServicemanDetailsForm
+                        selected_unit={selected_unit}
+                        unit_data={unit_data}
+                        set_dialog_settings={set_dialog_settings}
+                    />
+                </section>
+                {dialog_settings.displayed && (
+                    <Dialog
+                        message={dialog_settings.message}
+                        buttons={dialog_settings.buttons}
+                        line_props={dialog_settings.line_props}
+                        displayed={dialog_settings.displayed}
+                        onClickDialog={dialog_settings.onClickDialog}
+                        onClickDialogProps={dialog_settings.onClickDialogProps}
+                    />
+                )}
+            </main>
+        </>
+    )
+}
+
+
+export async function getServerSideProps({ params, req }) {
+    const selected_unit = params.unit.toUpperCase()
+
+    const { cookie } = req.headers
+
+    if (!cookie) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            },
+        }
+    }
+
+    const rootURL = process.env.NODE_ENV == "production" ? process.env.SERVER_URL : 'http://localhost:3000'
+    const response = await fetch(`${rootURL}/api/authentication/verifyauthentication?unitName=${selected_unit}&cookieType=UserAuth`, {
+        credentials: "include",
+        headers: { cookie },
+    })
+    
+    if (response.status == 401) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            },
+        }
+    }
+
+    if (response.status == 500) {
+        const error = await response.json()
+        return {
+            props: {
+                error: error.message
+            }
+        }
+    }
+
+    const unit_data_response = await fetch(`${rootURL}/api/unit-data?unit=${selected_unit}`, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    const {unit_data_dict: unit_data} = await unit_data_response.json()
+
+    return {
+        props: {
+            selected_unit,
+            unit_data
+        }
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,100 +1,169 @@
 import Head from 'next/head'
 import Select from 'react-select'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
 import { Header } from "src/components/header/header"
 import { Dialog } from "src/components/home/dialog"
 import prisma from '@/lib/prisma'
-import ServicemanDetailsForm from '@/src/components/forms/servicemandetailsform'
 
+export default function UserLogin({ units }) {
 
-export default function Home({units}) {
-  
-  const units_options = units.map(unit=>({label: unit, value: unit}))
+  const units_options = units.map(unit => ({ label: unit.name, value: unit }))
 
-  const [selected_unit, set_selected_unit] = useState('')
-  const [unit_data , set_unit_data] = useState()
+  const [selected_unit, set_selected_unit] = useState({ name: undefined, id: undefined })
+  const [input_password, set_input_password] = useState('')
+  const router = useRouter()
 
   const [dialog_settings, set_dialog_settings] = useState({
     "message": '',
     "buttons": [],
     "line_props": [],
     "displayed": false,
-    "onClickDialog": function(){return},
-    "onClickDialogProps": {}           
-})
+    "onClickDialog": function () { return },
+    "onClickDialogProps": {}
+  })
 
-  // Re-query the unit data whenever the selected unit changes
-  useEffect(()=>{
-    const fetchUnitData = async() => {
-      const unit_data_response = await fetch(`/api/unit-data?unit=${selected_unit}`, {
-          method: "GET",
-          headers: {
-              'Content-Type': 'application/json'
-          }
+  const closeDialogueBox = () => {
+    set_dialog_settings({
+      "message": '',
+      "buttons": [],
+      "line_props": [],
+      "displayed": false,
+      "onClickDialog": function () { return },
+      "onClickDialogProps": {}
+    })
+  }
+
+  const displayErrorMessage = async (error_message) => {
+
+    // This is necessary as the number of lines error messages consist of varies
+    const error_num_lines = error_message.split('\n').length
+
+    // If the error message consists of more than 1 line, align the text to the left instead
+    if (error_num_lines == 1) {
+      var error_lines_props = Array(error_num_lines).fill({
+        color: "#000000", font_size: "16px", text_align: "center", margin_right: "auto", margin_left: "auto"
       })
-      const unit_data_response_data  = await unit_data_response.json()
-      set_unit_data(unit_data_response_data.unit_data_dict)
-      console.log(unit_data_response_data.unit_data_dict)
+    } else {
+      var error_lines_props = Array(error_num_lines).fill({
+        color: "#000000", font_size: "16px", text_align: "left", margin_right: "auto", margin_left: "0"
+      })
     }
-    fetchUnitData()
 
-  }, [selected_unit])
+    set_dialog_settings({
+      "message":
+        `*Error*
+                ${error_message}`,
+      "buttons": [
+        { text: "Close", action: "exit", background: "#01a4d9", color: "#FFFFFF" }
+      ],
+      "line_props": [
+        { color: "#E60023", font_size: "25px", text_align: "center", margin_right: "auto", margin_left: "auto" },
+        ...error_lines_props],
+      "displayed": true,
+      "onClickDialog": closeDialogueBox,
+      "onClickDialogProps": { set_dialog_settings }
+    })
+  }
 
+  const onAttemptLogin = async () => {
+    // Input validation
+    if (!selected_unit.name || !selected_unit.id) {
+      displayErrorMessage("You have not selected a unit")
+      return
+    }
+
+    if (!input_password) {
+      displayErrorMessage("You have not keyed in the password")
+      return
+    }
+
+    const verification = await fetch(`/api/authentication/verifyuserpassword?current_password=${encodeURIComponent(input_password)}&unitID=${selected_unit.id}`, {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    if (!verification.ok) {
+      const response_data = await verification.json()
+      displayErrorMessage(response_data.message)
+      return
+    } else {
+      router.push(`/${selected_unit.name}`)
+    }
+  }
 
   return (
     <>
       <Head>
         <title>T&T Template Generator</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="robots" content="noindex,nofollow"/>
+        <meta name="robots" content="noindex,nofollow" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Header title={"T&T Template Generator"} subtitle={"Write Well-Written, Well-Organised, and Personalised T&Ts"} /> 
+        <Header title={"T&T Template Generator"} subtitle={"Write Well-Written, Well-Organised, and Personalised T&Ts"} />
         <section className="big-section">
           <h2 className="big-section-title">Select Your Unit</h2>
-          <Select 
-            instanceId="43b2b7e7-914f-4760-92b9-b1472920ef8e"
-            className="select-unit-main" 
-            onChange={option=>set_selected_unit(option.value)} 
-            options={units_options} 
-            placeholder={"Your Unit"}/>
+          <div style={{ display: "flex", flexDirection: "column", gap: "32px", alignItems: "center" }}>
+            <div style={{ display: "flex", flexDirection: "column", gap: "16px", alignItems: "center" }}>
+              <Select
+                instanceId="43b2b7e7-914f-4760-92b9-b1472920ef8e"
+                className="select-unit-admin"
+                onChange={option => set_selected_unit(option.value)}
+                options={units_options}
+                placeholder={"Your Unit"} />
+              <input style={{ width: "200px", height: "32px", fontSize: "16px", boxSizing: "border-box", border: "2px solid #ccc", borderRadius: "4px" }} onChange={(event) => { set_input_password(event.target.value) }} type={"password"} placeholder='Password'></input>
+            </div>
+            <div style={{ display: "flex", flexDirection: "column", gap: "48px", alignItems: "center" }}>
+              <button onClick={onAttemptLogin} style={{ backgroundColor: "#01a4d9", fontWeight: "bold", width: "90px", border: "0px", borderRadius: "20px", color: "white", fontSize: "20px" }}>Login</button>
+              <div style={{ display: "flex", flexDirection: "column", gap: "4px" }}>
+                <div style={{ fontWeight: "bold" }}>{"Don't know the password?"}</div>
+                <div style={{ maxWidth: "200px" }}> {"Please contact your unit's S1 dept as the password might have changed!"}</div>
+              </div>
+            </div>
+          </div>
         </section>
-        <section className="big-section">
-          <h2 className="big-section-title">Provide Your Serviceman&apos;s Details</h2>
-          <ServicemanDetailsForm
-            selected_unit={selected_unit}  
-            unit_data={unit_data}
-            set_dialog_settings={set_dialog_settings}
-          />
-        </section>  
         {dialog_settings.displayed && (
-                    <Dialog
-                        message={dialog_settings.message}
-                        buttons={dialog_settings.buttons}
-                        line_props={dialog_settings.line_props}
-                        displayed={dialog_settings.displayed} 
-                        onClickDialog={dialog_settings.onClickDialog}  
-                        onClickDialogProps={dialog_settings.onClickDialogProps}                 
-                    />
-                )}      
+          <Dialog
+            message={dialog_settings.message}
+            buttons={dialog_settings.buttons}
+            line_props={dialog_settings.line_props}
+            displayed={dialog_settings.displayed}
+            onClickDialog={dialog_settings.onClickDialog}
+            onClickDialogProps={dialog_settings.onClickDialogProps}
+          />
+        )}
       </main>
     </>
   )
 }
 
 
-export async function getServerSideProps(){
-  const unit_names_list = await prisma.Unit.findMany({
+export async function getServerSideProps() {
+  const units = await prisma.Unit.findMany({
     select: {
-        name: true
+      id: true,
+      name: true
     }
-  })  
-  const units = unit_names_list.map(obj=>obj.name)
-  units.sort()
+  })
+
+  units.sort((a, b) => {
+    const xString = String(a.name);
+    const yString = String(b.name);
+
+    if (xString < yString)
+      return -1;
+
+    if (xString > yString)
+      return 1;
+
+    return 0;
+  })
+
   return {
-      props: {
-        units
-      }
+    props: {
+      units
+    }
   }
 }
